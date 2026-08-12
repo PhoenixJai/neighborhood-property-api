b05e8c22b0ba41e394522369eeda1eeeb859eb8a
jaiden <springflowerwilson123@gmail.com>
2026-08-12 14:49:49 -0400

    Initial Spring Boot scaffold with Neighborhood entity and repository

---
+++ src/main/java/com/npapi/neighborhoodpropertyapi/NeighborhoodPropertyApiApplication.java
@@ -0,0 +1,13 @@
+package com.npapi.neighborhoodpropertyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborhoodPropertyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborhoodPropertyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npapi/neighborhoodpropertyapi/NeighborhoodPropertyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npapi.neighborhoodpropertyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborhoodPropertyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
